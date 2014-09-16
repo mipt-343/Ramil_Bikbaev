c791285bd18eb7dc455532f0aad328841f7728d2
vladimir-krasniy <vladimir-krasniy@mail.ru>
2014-09-16 15:27:37 +0400

    123 example added

---
+++ 123.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+int abs(int x)
+{
+x=(x>=0)?x:-x;
+return x;
+};
+double abs(double x)
+{
+x=(x>=0)?x:-x;
+return x;
+}
+float abs(float x)
+{
+x=(x>=0)?x:-x;
+return x;
+}
+int main()
+{
+    return 0;
+}
+
